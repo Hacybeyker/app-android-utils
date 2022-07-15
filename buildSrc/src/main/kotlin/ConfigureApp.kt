95a2649162c44725faed7ebeabdc9ec729e95b14
Carlos Osorio <hacybeyker@gmail.com>
2022-07-14 21:22:52 -0500

    update readme

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -2,7 +2,7 @@ object ConfigureApp {
     const val urlRepoDependencies = "https://maven.pkg.github.com/Hacybeyker/modules"
     const val groupId = "com.hacybeyker.android"
     const val artifactId = "utils"
-    const val version = "1.1.1"
+    const val version = "1.1.2"
     const val organization = "hacybeyker"
     const val projectName = "app-android-${artifactId}"
 }
